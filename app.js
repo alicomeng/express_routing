88d671540112e1757e818478a198c142aae9377e
alicomeng <ali.tayeh@yahoo.com>
2017-02-06 15:17:21 -0500

    adding new features: partials

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
 const express = require('express');
 var app = express();
-/*
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
-  res.send('Welcome to my ExpressJS routing assignment home page')
+  res.render('index');
 });
-
-app.get('/speak/pig', function (req, res) {
-  res.send('The pig says Oink!');
-});
-
-app.get('/speak/cow', function (req, res) {
-  res.send('The cow says Moo!');
-});
-
-app.get('/speak/dog', function (req, res) {
-  res.send('The dog says Woof..Woof!!');
-});
-*/
 //repeat hello 3 times
 app.get('/:greeting/:ti', function (req, res) {
   var num = Number(req.params.ti);
@@ -32,13 +21,16 @@ app.get('/:greeting/:ti', function (req, res) {
     {title:'Modren city planning', author:'Adam El Selman'},
 
   ];
-  res.render('greeting.ejs', {greetings: greetings, msg:msg, posts:posts});
+  res.render('greeting', {greetings: greetings, msg:msg, posts:posts});
 });
 
 app.get('/', function (req, res) {
-  res.render('index.ejs')
+  res.render('index');
 })
-// 404
+app.get('/ali', function (req, res) {
+  res.render('ali');
+})
+// catch all other undifined requests with 404
 app.get('*', function (req, res) {
   res.send('404 PAGE NOT FOUND!');
 });
